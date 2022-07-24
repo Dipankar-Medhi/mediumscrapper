9f2821799754d0ff5943d71e57e8b50da9baadeb
Dipankar-Medhi <dipumedhi99@gmail.com>
2022-07-24 18:03:24 +0530

    feature: add scrape by tag function

--- src/mediumscrapper/mediumscrapper.py
+++ src/mediumscrapper/mediumscrapper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def tag_scrape(tag, sort_by='latest'):
+def scrape_by_tag(tag, sort_by='latest'):
     """Get the titles and article links of the medium articles."""
     url = 'https://medium.com/tag/' + tag + '/' + sort_by
     data = requests.get(url).text
@@ -13,7 +13,7 @@ def tag_scrape(tag, sort_by='latest'):
     post_content = soup.findAll("article")
     for i in post_content:
         items = dict()
-        title = i.find("h1")
+        title = i.find("h2")
         link = i.find("a", {'aria-label': 'Post Preview Title'})
         items['title'] = title.text
         items['link'] = "https://medium.com" + link['href'].split("?")[0]
